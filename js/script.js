52677ede5a8f45d1e183f1ec063358917c14af49
VicKon <69324970+vickon16@users.noreply.github.com>
2021-04-13 08:35:03 +0100

    initial commit on project Loruki

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+
+$(document).ready(function(){
+
+    // var links = document.querySelectorAll(".nav-link");
+
+    // Array.from(links).forEach((item) => {
+    //     item.addEventListener("click", () => {
+    //         var current = document.getElementsByClassName("active");
+    //         current[0].className = current[0].className.replace(" active", "");
+    //         this.className += " active";
+    //     });
+    // })
+
+
+
+
+    // $(".nav-link").on("click", function(){
+    //     $(this).parent().find("ul li a.active").removeClass("active");
+    //     $(this).addClass("active");
+    // });
+    
+
+
+        
+
+})
+
+
+
